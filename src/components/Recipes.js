dc98599d2c9b001294b535eef3bfa230a8cca986
Irina O'Hara <iohara@rmoadmins-MacBook-Pro.local>
2019-04-26 15:09:47 -0600

    styling

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,12 +1,28 @@
 import React from "react";
 
 const Recipes = props => (
-	<div>
+	<div className="grid">
 		{props.recipes.map(recipe => {
 			return (
 				<div key={recipe.recipe_id}>
-					<img src={recipe.image_url} alt={recipe.title} />
-					<p>{recipe.title}</p>
+					<div className="recipes__box">
+						<img
+							className="recipes__box"
+							src={recipe.image_url}
+							alt={recipe.title}
+						/>
+						<div className="recipe__text">
+							<h5 className="recipes__title">
+								{recipe.title.length < 20
+									? `${recipe.title}`
+									: `${recipe.title.substring(0, 25)}...`}
+							</h5>
+							<p className="recipe__subtitle">
+								Publisher: <span> {recipe.publisher}</span>
+							</p>
+						</div>
+						<button className="recipe_buttons">View Recipe</button>
+					</div>
 				</div>
 			);
 		})}
